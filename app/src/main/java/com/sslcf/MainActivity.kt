5eab080eca3c06dcf2e3be3ce9c7328c179325d3
Munir Ahmad <munir.ahmad@digitify.com>
2022-01-25 12:01:35 +0500

    Sample app code updated

--- app/src/main/java/com/sslcf/MainActivity.kt
+++ app/src/main/java/com/sslcf/MainActivity.kt
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 import com.sslcf.api.ApiService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,19 +22,22 @@ import java.io.File
 import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = "MainActivity"
     private val secureEncodedKey = "pdqxnxEE2U3hPCsKk/nFhA=="
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseDatabase()
         //encryptFile()
-
         lifecycleScope.launch(Dispatchers.IO) {
             buildHttpClient()
         }
-
     }
 
-    private fun getHttpBuilder(): OkHttpClient.Builder {
+
+    private fun getHttpBuilder(secureEncodedKey: String): OkHttpClient.Builder {
         val okHttpClientBuilder = OkHttpClient.Builder()
         val keyStream = resources.openRawResource(R.raw.cloudflare_cer).readBytes()
         val decryptedFile = DecryptFile.decryptEncryptedFile(
@@ -45,7 +54,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun buildHttpClient() {
-        val okHttpClientBuilder = getHttpBuilder()
+        val okHttpClientBuilder = getHttpBuilder("")
         val retrofit = Retrofit.Builder()
             .baseUrl("https://test-mtls.yap.com/")
             .client(okHttpClientBuilder.build())
@@ -79,6 +88,25 @@ class MainActivity : AppCompatActivity() {
         //val fileData = FileUtils.readFile(File(filePath))
 
         val keyStream = resources.openRawResource(R.raw.dynamic).readBytes()
-        EncryptFile.encryptDownloadedFile(this, secureEncodedKey, file, keyStream)
+        EncryptFile.encryptFileAndSaveMemory(EncryptionUtils.generateSecretKey(secureEncodedKey), file, keyStream)
+    }
+
+    private fun firebaseDatabase() {
+
+        // Read from the database
+        Firebase.database.reference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<HashMap<String, String>>()
+                Log.d(TAG, "Value is: $value")
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
     }
 }
